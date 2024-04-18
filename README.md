5f389ec5d2d72c7647cbf7e39433c0a5023b6821
xfcs_tunluo <2602316693@qq.com>
2024-04-18 20:20:59 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,40 +1,51 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+```
+yarn create next-app .
 
-## Getting Started
+Would you like to use TypeScript? No
+Would you like to use ESLint? Yes
+Would you like to use Tailwind CSS?  Yes
+Would you like to use `src/` directory?  Yes
+Would you like to use App Router? (recommended) No
+Would you like to customize the default import alias (@/*)? No
 
-First, run the development server:
+ yarn add --dev prettier
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+ 前端组件库
+ https://web3ui.github.io/web3uikit/?path=/story/1-web3-parse-blockie--custom-seed
+
+ css组件
+ //按步骤
+https://tailwindcss.com/docs/installation
+
+ yarn add --dev tailwindcss postcss autoprefixer
+ yarn tailwindcss init -p
+
+ 安装PostCSS Language Support、Tailwind CSS IntelliSense插件
+
+ 一直没有前端显示的原因是没有在app.js文件引入主css文件。
+ import "../styles/globals.css"
+
+
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+```
+在ipfs服务器上执行文件
+1.yarn build
+2.yarn next export(可能出错时在next.config.mjg/js文件中添加“
+ .mjs:
+  images: {
+        unoptimized: true,
+    },
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+.js:
+module.exports = {
+    images: {
+        unoptimized: true
+    }
+}
+ )
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+3.导出后将out文件夹上传到ipfs上，设置固定-固定在本地节点-导出CID
+ipfs://CID
+可以在ipfs服务器上运行
+```
---
+++ components/LotteryEntrance.js
@@ -0,0 +1,119 @@
+//进入raffle合约的函数
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddress } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
+
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    // console.log(parseInt(chainIdHex))获取具体chainId值
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddress ? contractAddress[chainId][0] : null
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+
+    const dispatch = useNotification()
+
+    //Raffle.sol对应的功能函数
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "enterRaffle",
+        params: {},
+        msgValue: entranceFee,
+    })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getNumberOfPlayers",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    //与Raffle合约进行交互，获取执行后信息
+    async function updateUI() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayersFromCall = (await getNumberOfPlayers()).toString()
+        const recentWinnerFromCall = await getRecentWinner()
+        setEntranceFee(entranceFeeFromCall)
+        setNumPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            //读取raffle入场费
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    //“Enter Raffle”成功通知
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx) //处理一个新通知
+        updateUI()
+    }
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
+    return (
+        <div className="p-5">
+            Hi from lottery entrance!
+            {raffleAddress ? (
+                <div>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                        onClick={async function () {
+                            await enterRaffle({
+                                onSuccess: handleSuccess, //一旦成功，调用成功通知
+                            })
+                        }}
+                        disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
+                    </button>
+                    <div>
+                        Entrance Fee:{" "}
+                        {ethers.utils.formatUnits(ethers.BigNumber.from(entranceFee), "ether")} ETH
+                    </div>
+                    <div>Players:{numPlayers}</div>
+                    <div>Recent Winner:{recentWinner}</div>
+                </div>
+            ) : (
+                <div>No Raffle Address Deteched</div>
+            )}
+        </div>
+    )
+}
